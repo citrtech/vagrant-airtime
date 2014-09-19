a26c788582b891a7864b9d00edeebd731e3a88d4
brad winter <braddjwinter@gmail.com>
2014-09-18 18:25:13 -0700

    initial - airtime seems to install properly

---
+++ puphpet/files/startup-once/install airtime.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo 'deb http://apt.sourcefabric.org/ precise main' >> '/etc/apt/sources.list'
+sudo apt-get update
+sudo apt-get install -y --force-yes sourcefabric-keyring
+sudo apt-get update
+sudo apt-get install -y --force-yes airtime
