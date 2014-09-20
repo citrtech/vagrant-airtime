ab45cbab801860edbb5f930eb37afe933e0543e0
brad winter <braddjwinter@gmail.com>
2014-09-19 18:33:08 -0700

    removed dupe script

--- puphpet/files/startup-once/install airtime.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo 'deb http://apt.sourcefabric.org/ precise main' >> '/etc/apt/sources.list'
-sudo apt-get update
-sudo apt-get install -y --force-yes sourcefabric-keyring
-sudo apt-get update
-sudo apt-get install -y --force-yes airtime
